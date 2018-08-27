226fe9a0bcc9a7867973a1a2daf7e64e02cb2c05
Zalan <zlaval@gmail.com>
2018-08-27 20:24:50 +0200

    add interval based emitter endpoint

--- spring-boot2-demo/src/main/java/com/zalerix/blogsource/springboot2demo/controller/CarController.java
+++ spring-boot2-demo/src/main/java/com/zalerix/blogsource/springboot2demo/controller/CarController.java
@@ -3,12 +3,16 @@ package com.zalerix.blogsource.springboot2demo.controller;
 import com.zalerix.blogsource.springboot2demo.model.Car;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.UUID;
 
 @Api(description = "Car controller")
 @RestController
@@ -27,6 +31,12 @@ public class CarController {
     public Mono<Car> saveCar(@PathVariable String model) {
         Car car = new Car(model);
         return Mono.just(car);
+    }
+
+    @GetMapping(path = "flux", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<Car> streamCars() {
+        return Flux.interval(Duration.ofSeconds(2))
+                .map(x -> new Car(UUID.randomUUID().toString()));
     }
 
 }
