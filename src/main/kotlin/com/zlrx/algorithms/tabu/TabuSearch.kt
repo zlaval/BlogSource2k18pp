9c0f965b7db30ea8b80adda0dcba27a35f2bb987
zlaval <zlaval@gmail.com>
2021-05-10 11:15:14 +0200

    feat: refactor tabu search

--- src/main/kotlin/com/zlrx/algorithms/tabu/TabuSearch.kt
+++ src/main/kotlin/com/zlrx/algorithms/tabu/TabuSearch.kt
@@ -1,6 +1,5 @@
 package com.zlrx.algorithms.tabu
 
-import org.apache.commons.collections4.IteratorUtils
 import org.apache.commons.collections4.queue.CircularFifoQueue
 import kotlin.math.exp
 import kotlin.math.sin
@@ -10,10 +9,9 @@ import kotlin.math.sin
 //[-10,10] step 0.1
 const val NUMBER_OF_VALUES = 200
 const val ITERATIONS = 100_000
-
-//tabu list size
 const val TABU_TENURE = 400
 
+//https://academo.org/demos/3d-surface-plotter/?expression=exp(-x*x-y*y)*sin(x)&xRange=-3%2C%203&yRange=-3%2C%203&resolution=100
 val cost = { x: Double, y: Double ->
     exp(-x * x - y * y) * sin(x)
 }
@@ -23,8 +21,6 @@ data class State(
     val y: Double,
     val z: Double,
     val neighbours: MutableList<State> = mutableListOf()
-
-
 ) {
     override fun toString(): String {
         return "State(x=$x, y=$y, z=$z)"
@@ -50,71 +46,52 @@ data class State(
         return result
     }
 
-
 }
 
-class TabuList {
+class TabuSearch(private val states: Array<Array<State>>) {
 
     private val tabu = CircularFifoQueue<State>(TABU_TENURE)
-
-    fun add(state: State) {
-        tabu.add(state)
-    }
-
-    fun contains(state: State) = tabu.contains(state)
-
-    fun tabus() = IteratorUtils.toList(tabu.iterator())
-
-}
-
-class NeighbourSolution() {
-
-    fun bestNeighbour(states: Array<Array<State>>, neighbourStates: MutableList<State>, tabuStates: List<State>): State {
-        neighbourStates.removeAll(tabuStates)
-        if (neighbourStates.size == 0) {
-            return states[100][100]
-        }
-        var bestSolution = neighbourStates[0]
-        neighbourStates.forEach {
-            if (it.z < bestSolution.z) {
-                bestSolution = it
-            }
-        }
-
-        println(bestSolution)
-        return bestSolution
-    }
-
-}
-
-class TabuSearch(val states: Array<Array<State>>) {
-    private val tabus = TabuList()
-    private val neighbourSolution = NeighbourSolution()
 
     fun solve(initialState: State): State {
         var bestState = initialState
         var currentState = initialState
-        var iterations = 0
-        while (iterations < ITERATIONS) {
-            val candidates = currentState.neighbours
-            val tabu = tabus.tabus()
-            val bestNeighbour = neighbourSolution.bestNeighbour(states, candidates, tabu)
+
+        for (i in 0 until ITERATIONS) {
+            val candidates = currentState.neighbours.filter { !tabu.contains(it) }
+            val bestNeighbour = bestNeighbour(candidates)
             if (bestNeighbour.z < bestState.z) {
                 bestState = bestNeighbour
             }
-            tabus.add(currentState)
+            tabu.add(currentState)
             currentState = bestNeighbour
-            iterations++
         }
+
         return bestState
     }
 
-
+    private fun bestNeighbour(candidates: List<State>): State {
+        return if (candidates.isEmpty()) {
+            println("All elements around are tabu, jump back to start point.")
+            states[NUMBER_OF_VALUES / 2][NUMBER_OF_VALUES / 2]
+        } else {
+            val bestSolution = candidates.reduce { acc, state -> if (state.z < acc.z) state else acc }
+            println(bestSolution)
+            bestSolution
+        }
+    }
 }
 
 fun main() {
+    val grid = createGrid()
+    val center = grid[NUMBER_OF_VALUES / 2][NUMBER_OF_VALUES / 2]
+    val tabuSearch = TabuSearch(grid)
+    val result = tabuSearch.solve(center)
+    println(result)
+}
 
-    val states: Array<Array<State>> = Array(NUMBER_OF_VALUES) { x ->
+
+fun createGrid(): Array<Array<State>> {
+    val grid: Array<Array<State>> = Array(NUMBER_OF_VALUES) { x ->
         Array(NUMBER_OF_VALUES) { y ->
             val xc = (x - 100) / 10.0
             val yc = (y - 100) / 10.0
@@ -122,22 +99,14 @@ fun main() {
         }
     }
 
-    (0 until NUMBER_OF_VALUES).forEach {
-        states[it][0].neighbours.add(states[it][1])
-        states[it][199].neighbours.add(states[it][198])
-        states[0][it].neighbours.add(states[1][it])
-        states[199][it].neighbours.add(states[198][it])
-        if (it < NUMBER_OF_VALUES - 1 && it > 0)
-            (1 until NUMBER_OF_VALUES - 1).forEach { j ->
-                states[it][j].neighbours.add(states[it - 1][j])
-                states[it][j].neighbours.add(states[it + 1][j])
-                states[it][j].neighbours.add(states[it][j - 1])
-                states[it][j].neighbours.add(states[it][j + 1])
-            }
+    (0 until NUMBER_OF_VALUES).forEach { x ->
+        (0 until NUMBER_OF_VALUES).forEach { y ->
+            if (x > 0) grid[x][y].neighbours.add(grid[x - 1][y])
+            if (x < NUMBER_OF_VALUES - 1) grid[x][y].neighbours.add(grid[x + 1][y])
+            if (y > 0) grid[x][y].neighbours.add(grid[x][y - 1])
+            if (y < NUMBER_OF_VALUES - 1) grid[x][y].neighbours.add(grid[x][y + 1])
+        }
     }
 
-    val tabuSearch = TabuSearch(states)
-    val result = tabuSearch.solve(states[100][100])
-    println(result)
-
+    return grid
 }
